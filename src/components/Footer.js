52904664100f06aed2e421ce131f2699c3f718e9
gohwan <hg_07@naver.com>
2024-02-13 16:15:32 +0900

    fix: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
+import Image from "next/image"; // 로고 이미지를 사용하려면 이 주석을 해제하세요.
 // components/Footer.js
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-gray-400 py-6">
+    <footer className="bg-white text-black-400 py-6">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 gap-4 text-sm text-left">
           <div>
@@ -36,6 +37,60 @@ export default function Footer() {
               </a>
             </p>
           </div>
+        </div>
+        <div className="flex flex-wrap justify-start items-center pt-10 gap-2 sm:gap-4 md:gap-16">
+          {/* 로고 이미지 예시입니다. 실제 경로로 교체해 주세요. */}
+          <Image
+            src="/f2.png"
+            alt="로고"
+            width={100}
+            height={100}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+          {/* 각 Image에 적용된 className은 반응형 마진을 위한 것입니다. */}
+          <Image
+            src="/pngwinga.png"
+            alt="로고"
+            width={100}
+            height={100}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+          <Image
+            src="/f4.png"
+            alt="로고"
+            width={140}
+            height={100}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+          <Image
+            src="/f5.png"
+            alt="로고"
+            width={130}
+            height={120}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+          <Image
+            src="/f6.png"
+            alt="로고"
+            width={140}
+            height={120}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+
+          <Image
+            src="/pngegg.png"
+            alt="로고"
+            width={100}
+            height={120}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
+          <Image
+            src="/f1.png"
+            alt="로고"
+            width={120}
+            height={120}
+            className="mb-2 sm:mb-4 md:mb-6"
+          />
         </div>
       </div>
     </footer>
