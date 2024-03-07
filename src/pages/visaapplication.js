a137bfb9d5e5ccda26e05de2a43f7e0659f9ee49
gohwan <hg_07@naver.com>
2024-03-08 03:48:12 +0900

    dd

--- src/pages/visaapplication.js
+++ src/pages/visaapplication.js
@@ -185,22 +185,22 @@ const VisaApplicationForm = () => {
     setIsLoading(true); // 로딩 시작
     proceedToPaymentSuccess();
 
-    const emailResponse = await fetch("/api/sendEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(visaFormData), // 전체 폼 데이터를 JSON으로 변환
-    });
+    // const emailResponse = await fetch("/api/sendEmail", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(visaFormData), // 전체 폼 데이터를 JSON으로 변환
+    // });
     try {
       const paymentResult = await initiatePayment(IMP_UID, paymentParams);
-      // const emailResponse = await fetch("/api/sendEmailPc", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(visaFormData), // 전체 폼 데이터를 JSON으로 변환
-      // });
+      const emailResponse = await fetch("/api/sendEmailPc", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(visaFormData), // 전체 폼 데이터를 JSON으로 변환
+      });
 
       console.log("결제 및 검증 성공: ", paymentResult.message);
 
